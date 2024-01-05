0a74b14519b08146d6685e0f40159442a50734e2
user <justlivefree>
2024-01-05 22:20:13 +0500

    updated

--- tools/tools.py
+++ tools/tools.py
@@ -1,9 +1,8 @@
 def to_binary(data: int | str):
     result = ''
     if isinstance(data, int):
-        result = bin(data)
-        result = result[result.index('b') + 1:]
-        # result += '0' * (8 - len(save)) + save
+        save = bin(data)[2:]
+        result += '0' * (8 - len(save)) + save
     elif isinstance(data, str):
         for i in data:
             save = bin(ord(i))[2:]
@@ -11,22 +10,15 @@ def to_binary(data: int | str):
     return result
 
 
-def bw_xor(a: str | int, b: str | int):
-    a = to_binary(a) if isinstance(a, int) else a
-    b = to_binary(b) if isinstance(b, int) else b
+def bw_xor(a: str, b: str):
     x, y = sorted((a, b), key=lambda val: len(val))
     result = ''
     for i in range(1, len(x) + 1):
         result += str(int(x[-i] != y[-i]))
-    s = y[:len(y) - len(x)] + result[::-1]
-    # if s[0] == 'b':
-    #     print(a, b)
-    #     print(len(s[1:]))
-    #     return s[1:]
-    return s
+    return y[:len(y) - len(x)] + result[::-1]
 
 
-def bit_mode(plaintext: str | int, mode: int = 64):
+def bit_mode(plaintext: str, mode: int = 64):
     b_text = to_binary(plaintext)
     chunk = 0
     if (len_btext := len(b_text)) <= mode:
@@ -40,9 +32,6 @@ def bit_mode(plaintext: str | int, mode: int = 64):
 def left_shift(text: str, shift: int) -> str:
     for _ in range(shift):
         text = text[-1] + text[:-1]
-    if text[1] == 'b':
-        print('left shift')
-        print(text)
     return text
 
 
